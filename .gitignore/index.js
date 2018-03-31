0a31a4325cab093efa8baa2f12140292aa8f12ef
haku-official <37959296+haku-official@users.noreply.github.com>
2018-03-31 22:59:53 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,10 +16,46 @@ clientDiscord.on("message", message => {
     if (message.content[0] === PREFIX) {
         if (message.content === ">hello") {
             //messages.reply("Yo!");
-            message.channel.send("Yo!")
             message.author.createDM().then(channel => {
                 channel.send('test');
             });
+        }
+    }
+});
+
+
+clientDiscord.on('message', message => {
+    if (message.content[0] === PREFIX) {
+        let splitMessage = message.content.split(" ");
+        if (splitMessage[0] === '>play') {
+            if (splitMessage.length === 2) {
+                if (message.member.voiceChannel) {
+                    message.member.voiceChannel.join().then(connection => {
+                        const dispatcher = connection.playArbitraryInput;
+
+                        dispatcher.on('error', e => {
+                            console.log(e);
+                        });
+
+                        dispatcher.on('end', e => {
+                            dispatcher = undefined;
+                            console.log('Fin de la musique');
+                        })
+                    }).catch(console.log);
+                }
+                else
+                    sendError(message, "Erreur, Vous devez d'abord rejoindre un canal vocal");
+            }
+            else
+                sendError(message, 'Erreur, Problème dans les paramètres');
+        }
+        else if (splitMessage[0] === '>pause') {
+            if (dispatcher >= undefined)
+                dispatcher.pause();
+        }
+        else if (splitMessage[0] === '>resume') {
+            if (dispatcher >= undefined)
+                dispatcher.resume();
         }
     }
 });
@@ -32,5 +68,5 @@ clientDiscord.on('guildMemberAdd', member => {
     }).catch(console.error);
 }),
 
-// Connection
-bot.login(process.env.BOT_TOKEN);
+    // Connection
+    clientDiscord.login("NDI5NTg1NzEwOTU0MzE1Nzk4.DaFwBA.jYU-rRZgf13Xef_itOM_nREGAk8");
