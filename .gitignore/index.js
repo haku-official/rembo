563ddeb93c5a0b6363fdc1b52a7fdf6276bbc3ff
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-01 22:39:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -31,7 +31,7 @@ clientDiscord.on('message', message => {
             if (splitMessage.length === 2) {
                 if (message.member.voiceChannel) {
                     message.member.voiceChannel.join().then(connection => {
-                        const dispatcher = connection.playArbitraryInput;
+                        connection.playArbitraryInput;
 
                         dispatcher.on('error', e => {
                             console.log(e);
