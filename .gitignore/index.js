fd4dc0ae3f13518a2bc6c5923a67cc0ebe00388a
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-04 16:25:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -23,6 +23,8 @@ clientDiscord.on("message", message => {
     }
 });
 
+
+
 clientDiscord.on("guildMemberAdd", (member) => {
     var chBienvenue = member.guild.channels.find('id', '430789272652480512');
     if (chBienvenue != null) {
@@ -32,13 +34,12 @@ clientDiscord.on("guildMemberAdd", (member) => {
 
         member.user.send(embed);
         chBienvenue.send("m e s s a g e    de   b i e n v e n u e      ici");
+        
+        var role = member.guilds.roles.find("name","ÉTRANGER");
+        member.addRole(role)
     }
 })
   
-clientDiscord.on('guildMemberAdd', (member) => {
-    var role = member.guilds.roles.find("name","ÉTRANGER");
-    member.addRole(role)
-})    
 
 // Connection
 clientDiscord.login("NDI5NTg1NzEwOTU0MzE1Nzk4.DaY_pQ.BCp9H_ufubFtW4ms7hKAIko5atM");
