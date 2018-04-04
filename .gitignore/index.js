b87944612ce3c4766e7a7d3af398086e7dc3a4e8
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-04 12:35:31 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,17 +2,17 @@
 const discord = require("discord.js");
 
 /* VARIABLES */
-const clientDiscord = new discord.Client();
+const bot = new discord.Client();
 const PREFIX = ">";
 
 /* EVENEMENTS */
 var dispatcher;
 
-clientDiscord.on("ready", () => {
+bot.on("ready", () => {
     console.log("Prêt à travailler !");
 });
 
-clientDiscord.on("message", message => {
+bot.on("message", message => {
     if (message.content[0] === PREFIX) {
         if (message.content === ">hello") {
             //messages.reply("Yo!");
@@ -23,26 +23,24 @@ clientDiscord.on("message", message => {
     }
 });
 
-clientDiscord.on('guildMemberAdd', member => {
-    message.channel.send({
-        embed: {
-            color: 3447003,
-            author: {
-                name: bot.user.username,
-                icon_url: bot.user.avatarURL
-            },
-            title: 'Nouvel arrivant',
-            fields: [{
-                name: 'Bienvenue ${member} sur le serveur Majiyusekai',
-                value: 'Je te conseille, avant de commencer ton aventure, de faire un tour dans le salon #règle.',
-            },
-            {
-                name: 'Bonne continuation'
-            }]
-
-        }
+bot.on("guildMemberAdd", (member) => {
+    var chBienvenue = member.guild.channels.find('id', '430789272652480512');
+            if (chBienvenue != null) {
+                var embed = new Discord.RichEmbed()
+                    .setColor("#ff3399")
+                    .setTitle("Bienvenue " + member.user.username);
+  
+  member.user.send(embed);
+                member.user.send(welcome);
+                chBienvenue.send("m e s s a g e    de   b i e n v e n u e      ici");
     }
+})
 
+    bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue' + member.displayName);
+    }).catch(console.error);
+})
 
     // Connection
-    clientDiscord.login("NDI5NTg1NzEwOTU0MzE1Nzk4.DaOhqw.ldt8djWcTW7bvfQYVC4X3cJ2J7g");
+    clientDiscord.login("NDI5NTg1NzEwOTU0MzE1Nzk4.DaFwBA.jYU-rRZgf13Xef_itOM_nREGAk8");
